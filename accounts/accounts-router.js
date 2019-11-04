462430b9b0937db45f2a8e54eb95bf1da0571fb4
dominique maack <dominiquemaack@gmail.com>
2019-11-04 15:27:42 -0700

    UPDATE and DELETE requests working -- MVP

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -23,15 +23,42 @@ router.get('/', (req,res) => {
     .then(accounts => {
         res.status(200).json(accounts)
     })
-    .catch(() => {
+    .catch(err => {
         res.status(500).json({ error: 'Failed to get accounts from database'})
     })
 })
 
 
 // UPDATE Requests
+router.put('/:id', (req,res) => {
+    const id = req.params.id;
+    const updates= req.body;
+
+    db('accounts')
+    .where({id: req.params.id})
+    .update(updates)
+    .then(success => {
+        res.status(201).json({ message: `You successfully updated the account with ID ${id}`})
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Failed to update you account changes'})
+    })
+})
 
 // DELETE Requests
+router.delete('/:id', (req,res) => {
+    const id = req.params.id
+
+    db('accounts')
+    .where({id: req.params.id})
+    .del()
+    .then(success => {
+        res.status(201).json({ message: `Account with ID ${id} was successfully deleted`})
+    })
+    .catch(err => {
+        res.status(500).json({ error: `Failed to delete the account with ID ${id}`})
+    })
+})
 
 
 module.exports = router
